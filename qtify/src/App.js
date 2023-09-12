f77f8b52dd3b42023a73cf1b15f88f59e9603fca
mahima-purohit <mahimapurohit4991@gmail.com>
2023-09-13 00:04:55 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Hero from './components/Hero/Hero';
+import Card from './components/Card/Card';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Card />
     </div>
   );
 }
